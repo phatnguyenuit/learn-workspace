21766394b99390b48badf706be4114d3e196662c
phatnguyenuit <phatnt.uit@gmail.com>
2020-07-09 17:24:33 +0700

    Update package-a

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -1,1 +1,17 @@
-console.log("package-a")
+const yargs = require('yargs');
+
+yargs
+  .command({
+    command: 'get',
+    describe: 'make a get HTTP request',
+    builder: function (yargs) {
+      return yargs.option('u', {
+        alias: 'url',
+        describe: 'the URL to make an HTTP request to',
+      });
+    },
+    handler: function (argv) {
+      console.log(`Requesting url: ${argv.url}`);
+    },
+  })
+  .help().argv;
